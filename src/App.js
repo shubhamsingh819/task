0a1cec0ee4e3fc6d72bed4b27cc4a745ef445bc9
Shubham <shubhamsingh8199@gmail.com>
2024-09-07 14:37:02 +0530

    fix: addding comment

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [filteredUsers, setFilteredUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [filterZipCode, setFilterZipCode] = useState("All");
-  const [zipcodes, setZipcodes] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [sortOrder, setSortOrder] = useState(null);
+  const [users, setUsers] = useState([]); // storing list of users
+  const [filteredUsers, setFilteredUsers] = useState([]); // filtering users
+  const [loading, setLoading] = useState(false); // loading whether data is being fetched
+  const [filterZipCode, setFilterZipCode] = useState("All"); // storing selected zipcode
+  const [zipcodes, setZipcodes] = useState([]); // storing list of unique zip code for filter
+  const [searchTerm, setSearchTerm] = useState(""); // storing currers search term for filtering  the name
+  const [sortOrder, setSortOrder] = useState(null); // storing the current sorting order (asc, dsc or null)
 
   useEffect(() => {
     fetchUserData();
@@ -37,6 +37,7 @@ function App() {
     fetchUserData();
   };
 
+  // updaing the search term and filtering based on the input
   const handleSearchChange = (e) => {
     const value = e.target.value.toLowerCase();
     setSearchTerm(value);
@@ -50,6 +51,7 @@ function App() {
     }
   };
 
+  // updating the filter based on selected zipcodes
   const handleFilterChange = (e) => {
     const value = e.target.value;
     setFilterZipCode(value);
@@ -60,8 +62,8 @@ function App() {
     }
   };
 
-  console.log(zipcodes, "zipcodes");
 
+//  sorting the filterdedUsers based on the column clicked (name or address.zipcode) (asc , dsc)
   const handleSort = (column) => {
     let sortedUsers = [...filteredUsers];
     if (sortOrder === "asc") {
